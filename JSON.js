9ad8c8f69976f7f7a7a6483b11b6d1e62b7126b9
thirdy454 <gayares@gmail.com>
2022-04-05 19:08:20 +0800

    convert to JSON

---
+++ JSON.js
@@ -0,0 +1,22 @@
+const FullName = [
+
+    {
+        Name: 'Thirdy',
+        Age: '20',
+        favorite: 'bikers'
+    },
+    {
+        Name: 'jonny',
+        Age: '25',
+        favorite: 'billiard'
+    },
+    {
+        Name: 'renan',
+        Age: '21',
+        favorite: 'gym'
+    },
+];
+
+const jsonCOnvert = JSON.stringify(FullName);
+
+console.log(jsonCOnvert);
