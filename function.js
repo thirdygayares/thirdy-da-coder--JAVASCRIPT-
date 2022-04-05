6d113b799dd0dfd3572562d5b2ae0591ff9d39d0
thirdy454 <gayares@gmail.com>
2022-04-05 19:26:23 +0800

    different fuction of js

---
+++ function.js
@@ -0,0 +1,19 @@
+
+function getSum(x,y){
+    return x+y;
+}
+
+
+//use this if you have one formula
+const getSum2 = (x,y) => x+y; 
+
+//use this if you more than one formula
+const getSum3 = (x,y) => { 
+    return x+y;
+}; 
+
+console.log(getSum(2,1));
+console.log(getSum(6,4));
+console.log(getSum2(16,4));
+console.log(getSum3(161,4));
+
